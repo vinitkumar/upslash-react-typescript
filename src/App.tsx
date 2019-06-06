e3a8accc5bd3cc8013c9ee6f4dafca09cc63e182
Vinit Kumar <vinit@getchip.uk>
2019-06-06 22:42:15 +0530

    fix: get the app working

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React, {useState, useEffect, FunctionComponent} from 'react';
 import logo from './logo.svg';
 import { FaPlay, FaPause, FaForward, FaBackward } from "react-icons/fa";
 import './App.css';
-import ImagesList from './ImagesList';
 import GetData from './Api';
 const API_URL = `https://api.unsplash.com/photos/?client_id=0a52c7eede6320232b60ced9e2a2147a79dda95dfd63d2454fc7b71a9c09a634`;
+
+type ImgValue = {
+  urls: {raw: string, thumb: string},
+  alt_description: string,
+
+}
 
 const App: React.FunctionComponent = () => {
   // console.log(GetData(API_URL));
@@ -21,14 +26,14 @@ const App: React.FunctionComponent = () => {
     }, []);
     return (
     <div className="App">
-      <FaPlay />
-      <FaPause />
-      <FaForward />
-      <FaBackward />
       <h1> Image loading component trial </h1>
       <div className="image-container">
-        <ImagesList images={imageData} />
+        {imageData && imageData.map((value: ImgValue, key) => {
+          return <img src={value.urls.thumb} alt={value.alt_description} key={String(key)}/>
+        })}
       </div>
+      <FaBackward />
+      <FaForward />
     </div>
   );
 }
